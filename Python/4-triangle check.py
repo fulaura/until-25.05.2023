0ac479fb1d48509aa5c562eb574f247511de9e45
Bekzat <91748401+fulaura@users.noreply.github.com>
2023-06-01 21:08:39 +0600

    until-25.05.2023 /Python

--- Python/4-triangle check.py
+++
@@ -1,7 +0,0 @@
-a=int(input("side length 1"))
-b=int(input("side length 2"))
-c=int(input("side length 3"))
-if (a**2)+(b**2)==(c**2) or (a**2)+(c**2)==(b**2) or (b**2)+(c**2)==(a**2):
-    print("true")
-else:
-    print("false")
